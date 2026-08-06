e66ca0ea31e6108e2df8022b3d9d6a9fe6ea7272
Ami <d.dimaaaaaa@ya.ru>
2026-08-06 21:13:42 +0300

    Удалил лишнее

--- shared/src/commonMain/kotlin/org/example/project/features/addCoffee/ui/states/AddCoffeeScreenUiEvents.kt
+++
@@ -1,7 +0,0 @@
-package org.example.project.features.addCoffee.ui.states
-
-sealed interface AddCoffeeScreenUiEvents {
-    object PickPhoto : AddCoffeeScreenUiEvents
-    object NavigateToCoffeeScreen : AddCoffeeScreenUiEvents
-    data class ShowAiError(val message: String) : AddCoffeeScreenUiEvents
-}
