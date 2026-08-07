0c5e734f07d0e1c65309fd082d263a734890ec54
Ami <d.dimaaaaaa@ya.ru>
2026-08-07 23:12:38 +0300

    Удалил лишнее

--- shared/src/commonMain/kotlin/org/example/project/features/savedCoffee/ui/states/CoffeeScreenUiState.kt
+++
@@ -1,7 +0,0 @@
-package org.example.project.features.savedCoffee.ui.states
-
-import org.example.project.core.domain.model.Coffee
-
-data class CoffeeScreenUiState(
-    val coffeeList: List<Coffee> = emptyList()
-)
