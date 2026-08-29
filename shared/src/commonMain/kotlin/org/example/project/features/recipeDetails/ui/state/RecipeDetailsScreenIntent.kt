f6ac8caf5a34cbdd84a5873d8b4cc76cffc926d2
Ami <d.dimaaaaaa@ya.ru>
2026-08-29 22:43:11 +0300

    Удалил лишнее

--- shared/src/commonMain/kotlin/org/example/project/features/recipeDetails/ui/state/RecipeDetailsScreenIntent.kt
+++
@@ -1,8 +0,0 @@
-package org.example.project.features.recipeDetails.ui.state
-
-import org.example.project.core.domain.model.Recipe
-
-sealed interface RecipeDetailsScreenIntent {
-    data class LoadRecipeDetails(val waterAmount: Int) : RecipeDetailsScreenIntent
-    data class SaveRecipeDetailsToRecents(val recipe: Recipe) : RecipeDetailsScreenIntent
-}
